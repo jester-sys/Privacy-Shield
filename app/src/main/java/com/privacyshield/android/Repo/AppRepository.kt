041a1fd1d5628fff1759eb665ad5a05567660ae1
Jester <115554090+jester-sys@users.noreply.github.com>
2025-08-24 03:02:48 +0530

    Create Lon CLick Screen

--- app/src/main/java/com/privacyshield/android/Repo/AppRepository.kt
+++ app/src/main/java/com/privacyshield/android/Repo/AppRepository.kt
@@ -58,11 +58,20 @@ class AppRepository @Inject constructor(
                     }
                 } ?: emptyList()
 
-                // ⚡ custom checks
                 val isSystemApp = (appsInfo.flags and ApplicationInfo.FLAG_SYSTEM) != 0
                 val isOemApp = (appsInfo.flags and ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0
-//                val isSideloaded = (appsInfo.flags and ApplicationInfo.FLAG_INSTALLED) != 0 &&
-//                        (appsInfo.sourceDir?.startsWith("/data/app/") == true)
+                val installerPackage = pm.getInstallerPackageName(appsInfo.packageName)
+                val isFromPlayStore = installerPackage == "com.android.vending"
+
+
+                val isSideloaded = !isFromPlayStore && !isSystemApp && !isOemApp
+
+
+                val isPlayStore = isFromPlayStore
+
+                val isUserApp = !isSystemApp && !isOemApp
+
+
 
 
 
@@ -73,14 +82,6 @@ class AppRepository @Inject constructor(
 
                 val sharedUserId = packageInfo.sharedUserId
                 val installerPackageName = pm.getInstallerPackageName(appsInfo.packageName)
-                val isFromPlayStore = installerPackageName == "com.android.vending"
-//                val installSource = when {
-//                    isSystemApp -> "System App"
-//                    isFromPlayStore -> "Play Store"
-//                    else -> "APK / Unknown"
-//                }
-                val isPlayStore = !isSystemApp && !isOemApp
-                val isSideloaded = !isSystemApp && !isOemApp && !isFromPlayStore
                 val isCloned = (appsInfo.packageName.contains(":")) // clone apps usually have colon suffix
                 val isActiveProfile = android.os.Process.myUid() / 100000 == appsInfo.uid / 100000
                 val isManagedProfile = (appsInfo.flags and ApplicationInfo.FLAG_ALLOW_CLEAR_USER_DATA) != 0
@@ -124,7 +125,7 @@ class AppRepository @Inject constructor(
                     isManagedProfile = isManagedProfile,
                     hasCustomBatterySetting = hasCustomBatterySetting,
                     isAccessibilityService = isAccessibilityService,
-                    isFromPlayStore = isPlayStore,
+                    isFromPlayStore = isSystemApp,
 
 
                 )
